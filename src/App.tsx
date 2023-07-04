ed0f03fe4c683214f3376c6f839ce05e0936eff8
Player Nguyên <daudaua3782@gmail.com>
2023-07-04 12:51:06 +0700

    refactor: change app browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import Home from "./components/Home/Home";
+import React, { Fragment } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Navbar from "./components/Navbar/Navbar";
 
 export default function App() {
   return (
-    <div className="px-12 py-24 flex flex-col gap-12">
-      <div>Change `./src/App` to modify the source code.</div>
-      <div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </div>
-    </div>
+    <BrowserRouter>
+      {/* Header navbar */}
+      <Navbar />
+
+      {/* Route switcher */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
