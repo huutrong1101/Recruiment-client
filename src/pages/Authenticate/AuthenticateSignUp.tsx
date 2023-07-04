e697ba419082e433826c67896cfbb2d637471ac8
Player Nguyên <daudaua3782@gmail.com>
2023-07-04 15:47:00 +0700

    fix: using new components

--- src/pages/Authenticate/AuthenticateSignUp.tsx
+++ src/pages/Authenticate/AuthenticateSignUp.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import classnames from "classnames";
 import { useForm } from "react-hook-form";
 import { EnvelopeIcon, LockClosedIcon } from "@heroicons/react/24/outline";
+import { Link } from "react-router-dom";
+import InputIcon from "../../components/InputIcon/InputIcon";
 
 export default function AuthenticateSignUp() {
   const { register, handleSubmit } = useForm();
@@ -11,7 +13,7 @@ export default function AuthenticateSignUp() {
   return (
     <form
       className={classnames(
-        `py-8 gap-4 items-center justify-center flex flex-col h-[400px]`,
+        `py-8 gap-4 items-center justify-center flex flex-col h-[600px]`,
         `bg-zinc-100 shadow-md`,
         `rounded-xl`
       )}
@@ -21,7 +23,7 @@ export default function AuthenticateSignUp() {
         <h1 className="text-xl font-semibold">Sign up</h1>
 
         {/* Input group with icons */}
-        <div
+        {/* <div
           className={classnames(
             `flex flex-row items-center gap-2 py-2`,
             `bg-white text-zinc-500`,
@@ -41,45 +43,29 @@ export default function AuthenticateSignUp() {
               `outline-none`
             )}
           />
-        </div>
+        </div> */}
 
-        <div
-          className={classnames(
-            `flex flex-row items-center gap-2 py-2`,
-            `bg-white text-zinc-500`,
-            `rounded-md`,
-            `border`
-          )}
-        >
-          <LockClosedIcon className={classnames(`w-[16px] ml-4`)} />
-          <input
-            type="password"
-            placeholder="password"
-            {...register("password")}
-            className={classnames(
-              `px-2 py-1`,
-              `font-light`,
-              `mr-4`,
-              `outline-none`
-            )}
-          />
-        </div>
+        <InputIcon
+          icon={<EnvelopeIcon />}
+          {...register("credentialId")}
+          placeholder="full name"
+        />
+
+        <InputIcon
+          icon={<LockClosedIcon />}
+          {...register("password")}
+          placeholder="password"
+        />
 
         {/* Remember Me */}
-        <div className="flex flex-row gap-4 w-full px-1 text-zinc-600">
+        <div className="flex flex-row gap-4 w-full text-zinc-600">
           <input type="checkbox" id="remember" />
-          <label htmlFor="remember">Remember me</label>
-        </div>
-
-        {/* Forgot password */}
-        <div className="Button w-44 h-10 bg-white bg-opacity-0 rounded-lg flex-col justify-center items-center inline-flex text-sm">
-          <div className="Basebutton px-3 py-1.5 justify-center items-center inline-flex">
-            <div className="Content justify-center items-center gap-2 flex">
-              <div className="Button text-emerald-800 font-semibold capitalize leading-7 tracking-wide">
-                Forget Password?
-              </div>
-            </div>
-          </div>
+          <label htmlFor="remember">
+            I'm agree with the{" "}
+            <b>
+              <Link to="/">Term and conditions</Link>
+            </b>
+          </label>
         </div>
 
         <button
@@ -91,7 +77,7 @@ export default function AuthenticateSignUp() {
           <div className="Basebutton px-5 py-2.5 justify-center items-center inline-flex">
             <div className="Content justify-center items-center gap-2 flex">
               <div className="Button text-white font-semibold capitalize leading-7 tracking-wide">
-                Login
+                Sign up
               </div>
             </div>
           </div>
