d65e2c88c95277525df1f4f0ce3439d8ce063eda
Player Nguyên <daudaua3782@gmail.com>
2023-07-06 08:57:11 +0700

    refactor: clean up redundant imports, route misspells

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
-import Navbar from "./components/Navbar/Navbar";
-import Login from "./pages/Authenticate/AuthenticateLogin";
-import classNames from "classnames";
+
 import Authenticate from "./pages/Authenticate/Authenticate";
 import AuthenticateLogin from "./pages/Authenticate/AuthenticateLogin";
 import AuthenticateSignUp from "./pages/Authenticate/AuthenticateSignUp";
 import VerifyEmail from "./pages/VerifyEmail/VerifyEmail";
-import Container from "./components/Container/Container";
 import UserAppLayout from "./components/UserAppLayout/UserAppLayout";
 
 export default function App() {
@@ -20,7 +17,7 @@ export default function App() {
           <Route path="auth" element={<Authenticate />}>
             <Route index path="login" element={<AuthenticateLogin />} />
             <Route index path="signup" element={<AuthenticateSignUp />} />
-            <Route index element={<Login />} />
+            <Route index element={<AuthenticateLogin />} />
           </Route>
           <Route path="/verify-email" element={<VerifyEmail />}></Route>
 
