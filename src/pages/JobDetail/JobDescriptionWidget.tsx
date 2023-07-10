0327c3e9ede625681a118dc54cc0a397644ab9d8
PlayerNguyen <daudaua3782@gmail.com>
2023-07-10 23:13:14 +0700

    feat: add Widget and Related Vacancies blocks

---
+++ src/pages/JobDetail/JobDescriptionWidget.tsx
@@ -0,0 +1,66 @@
+import { Fragment } from "react";
+import { Transition } from "@headlessui/react";
+import classNames from "classnames";
+
+interface JobDescriptionWidgetProps {
+  companyName: string;
+  jobRole: string;
+  publishDate: Date;
+
+  logo: {
+    src: any;
+    alt: string;
+  };
+}
+
+export default function JobDescriptionWidget({
+  companyName,
+  jobRole,
+  publishDate,
+  logo,
+}: JobDescriptionWidgetProps) {
+  return (
+    <Transition as={Fragment} show={true}>
+      <div
+        className={classNames(
+          `flex flex-row bg-white shadow-sm`,
+          `rounded-xl p-2`,
+          `gap-6 items-center`,
+          `border`
+        )}
+      >
+        {/* Logo */}
+        <div
+          className={classNames(
+            `w-2/12 flex flex-col items-center justify-center`
+          )}
+        >
+          <img
+            className={classNames(`min-w-[64px] w-1/2`)}
+            draggable={false}
+            src={logo.src}
+            alt={logo.alt}
+          />
+        </div>
+        {/* Information */}
+        <div className={classNames(`flex flex-row flex-1 `)}>
+          <div
+            className={classNames(`flex flex-col flex-1 font-semibold gap-1`)}
+          >
+            <span>{companyName}</span>
+            <span>{jobRole}</span>
+            <span className={classNames(`text-sm text-zinc-400`)}>
+              21 applicants
+            </span>
+          </div>
+          {/* Right */}
+
+          {/* TODO: change this to from date to date */}
+          <div className={`text-zinc-400 text-sm`}>
+            {publishDate.toDateString()}
+          </div>
+        </div>
+      </div>
+    </Transition>
+  );
+}
--- src/pages/JobDetail/JobDetail.tsx
+++ src/pages/JobDetail/JobDetail.tsx
@@ -10,6 +10,10 @@ import {
 import classNames from "classnames";
 import { useParams } from "react-router-dom";
 import { useState } from "react";
+import JobDescriptionWidget from "./JobDescriptionWidget";
+import Logo from "./../../../images/logo_FPT.png";
+import JobInformationCard from "./JobInformationCard";
+import JobCard from "../../components/JobCard/JobCard";
 
 export default function JobDetail() {
   const { jobId } = useParams();
@@ -38,111 +42,124 @@ export default function JobDetail() {
 
   return (
     <div className={classNames(`job-detail`)}>
-      {jobId}
       <div className={classNames(`flex flex-col md:flex-row gap-12`)}>
         {/* Left side description */}
-        <div className={classNames(`w-full md:w-8/12`)}>
+        <div className={classNames(`w-full md:w-8/12`, `flex flex-col gap-6`)}>
           {/* Widgets */}
-
+          <JobDescriptionWidget
+            companyName="FPT Software"
+            jobRole="Web Designer"
+            publishDate={new Date()}
+            logo={{ src: Logo, alt: "image" }}
+          />
           {/* Details */}
-          <div>
-            Job description
-            <p>
-              One disadvantage of Lorum Ipsum is that in Latin certain letters
-              appear more frequently than others - which creates a distinct
-              visual impression. Moreover, in Latin only words at the beginning
-              of sentences are capitalized. This means that Lorem Ipsum cannot
-              accurately represent, for example, German, in which all nouns are
-              capitalized. Thus, Lorem Ipsum has only limited suitability as a
-              visual filler for German texts. If the fill text is intended to
-              illustrate the characteristics of different typefaces. It
-              sometimes makes sense to select texts containing the various
-              letters and symbols specific to the output language.
-            </p>
-            <p>
-              One disadvantage of Lorum Ipsum is that in Latin certain letters
-              appear more frequently than others - which creates a distinct
-              visual impression. Moreover, in Latin only words at the beginning
-              of sentences are capitalized. This means that Lorem Ipsum cannot
-              accurately represent, for example, German, in which all nouns are
-              capitalized. Thus, Lorem Ipsum has only limited suitability as a
-              visual filler for German texts. If the fill text is intended to
-              illustrate the characteristics of different typefaces. It
-              sometimes makes sense to select texts containing the various
-              letters and symbols specific to the output language.
-            </p>
-            <p>
-              One disadvantage of Lorum Ipsum is that in Latin certain letters
-              appear more frequently than others - which creates a distinct
-              visual impression. Moreover, in Latin only words at the beginning
-              of sentences are capitalized. This means that Lorem Ipsum cannot
-              accurately represent, for example, German, in which all nouns are
-              capitalized. Thus, Lorem Ipsum has only limited suitability as a
-              visual filler for German texts. If the fill text is intended to
-              illustrate the characteristics of different typefaces. It
-              sometimes makes sense to select texts containing the various
-              letters and symbols specific to the output language.
-            </p>
-            <p>
-              One disadvantage of Lorum Ipsum is that in Latin certain letters
-              appear more frequently than others - which creates a distinct
-              visual impression. Moreover, in Latin only words at the beginning
-              of sentences are capitalized. This means that Lorem Ipsum cannot
-              accurately represent, for example, German, in which all nouns are
-              capitalized. Thus, Lorem Ipsum has only limited suitability as a
-              visual filler for German texts. If the fill text is intended to
-              illustrate the characteristics of different typefaces. It
-              sometimes makes sense to select texts containing the various
-              letters and symbols specific to the output language.
-            </p>
-            <p>
-              One disadvantage of Lorum Ipsum is that in Latin certain letters
-              appear more frequently than others - which creates a distinct
-              visual impression. Moreover, in Latin only words at the beginning
-              of sentences are capitalized. This means that Lorem Ipsum cannot
-              accurately represent, for example, German, in which all nouns are
-              capitalized. Thus, Lorem Ipsum has only limited suitability as a
-              visual filler for German texts. If the fill text is intended to
-              illustrate the characteristics of different typefaces. It
-              sometimes makes sense to select texts containing the various
-              letters and symbols specific to the output language.
-            </p>
-            <p>
-              One disadvantage of Lorum Ipsum is that in Latin certain letters
-              appear more frequently than others - which creates a distinct
-              visual impression. Moreover, in Latin only words at the beginning
-              of sentences are capitalized. This means that Lorem Ipsum cannot
-              accurately represent, for example, German, in which all nouns are
-              capitalized. Thus, Lorem Ipsum has only limited suitability as a
-              visual filler for German texts. If the fill text is intended to
-              illustrate the characteristics of different typefaces. It
-              sometimes makes sense to select texts containing the various
-              letters and symbols specific to the output language.
-            </p>
-            <p>
-              One disadvantage of Lorum Ipsum is that in Latin certain letters
-              appear more frequently than others - which creates a distinct
-              visual impression. Moreover, in Latin only words at the beginning
-              of sentences are capitalized. This means that Lorem Ipsum cannot
-              accurately represent, for example, German, in which all nouns are
-              capitalized. Thus, Lorem Ipsum has only limited suitability as a
-              visual filler for German texts. If the fill text is intended to
-              illustrate the characteristics of different typefaces. It
-              sometimes makes sense to select texts containing the various
-              letters and symbols specific to the output language.
-            </p>
-            <p>
-              One disadvantage of Lorum Ipsum is that in Latin certain letters
-              appear more frequently than others - which creates a distinct
-              visual impression. Moreover, in Latin only words at the beginning
-              of sentences are capitalized. This means that Lorem Ipsum cannot
-              accurately represent, for example, German, in which all nouns are
-              capitalized. Thus, Lorem Ipsum has only limited suitability as a
-              visual filler for German texts. If the fill text is intended to
-              illustrate the characteristics of different typefaces. It
-              sometimes makes sense to select texts containing the various
-              letters and symbols specific to the output language.
-            </p>
+          <div
+            className={classNames(
+              `border bg-white shadow-sm rounded-xl`,
+              `px-8 py-8`,
+              `text-justify`
+            )}
+          >
+            <div>
+              <h1 className="text-2xl font-semibold">Job description</h1>
+              <p>
+                One disadvantage of Lorum Ipsum is that in Latin certain letters
+                appear more frequently than others - which creates a distinct
+                visual impression. Moreover, in Latin only words at the
+                beginning of sentences are capitalized. This means that Lorem
+                Ipsum cannot accurately represent, for example, German, in which
+                all nouns are capitalized. Thus, Lorem Ipsum has only limited
+                suitability as a visual filler for German texts. If the fill
+                text is intended to illustrate the characteristics of different
+                typefaces. It sometimes makes sense to select texts containing
+                the various letters and symbols specific to the output language.
+              </p>
+              <br />
+              <p>
+                One disadvantage of Lorum Ipsum is that in Latin certain letters
+                appear more frequently than others - which creates a distinct
+                visual impression. Moreover, in Latin only words at the
+                beginning of sentences are capitalized. This means that Lorem
+                Ipsum cannot accurately represent, for example, German, in which
+                all nouns are capitalized. Thus, Lorem Ipsum has only limited
+                suitability as a visual filler for German texts. If the fill
+                text is intended to illustrate the characteristics of different
+                typefaces. It sometimes makes sense to select texts containing
+                the various letters and symbols specific to the output language.
+              </p>
+              <p>
+                One disadvantage of Lorum Ipsum is that in Latin certain letters
+                appear more frequently than others - which creates a distinct
+                visual impression. Moreover, in Latin only words at the
+                beginning of sentences are capitalized. This means that Lorem
+                Ipsum cannot accurately represent, for example, German, in which
+                all nouns are capitalized. Thus, Lorem Ipsum has only limited
+                suitability as a visual filler for German texts. If the fill
+                text is intended to illustrate the characteristics of different
+                typefaces. It sometimes makes sense to select texts containing
+                the various letters and symbols specific to the output language.
+              </p>
+              <p>
+                One disadvantage of Lorum Ipsum is that in Latin certain letters
+                appear more frequently than others - which creates a distinct
+                visual impression. Moreover, in Latin only words at the
+                beginning of sentences are capitalized. This means that Lorem
+                Ipsum cannot accurately represent, for example, German, in which
+                all nouns are capitalized. Thus, Lorem Ipsum has only limited
+                suitability as a visual filler for German texts. If the fill
+                text is intended to illustrate the characteristics of different
+                typefaces. It sometimes makes sense to select texts containing
+                the various letters and symbols specific to the output language.
+              </p>
+              <p>
+                One disadvantage of Lorum Ipsum is that in Latin certain letters
+                appear more frequently than others - which creates a distinct
+                visual impression. Moreover, in Latin only words at the
+                beginning of sentences are capitalized. This means that Lorem
+                Ipsum cannot accurately represent, for example, German, in which
+                all nouns are capitalized. Thus, Lorem Ipsum has only limited
+                suitability as a visual filler for German texts. If the fill
+                text is intended to illustrate the characteristics of different
+                typefaces. It sometimes makes sense to select texts containing
+                the various letters and symbols specific to the output language.
+              </p>
+              <p>
+                One disadvantage of Lorum Ipsum is that in Latin certain letters
+                appear more frequently than others - which creates a distinct
+                visual impression. Moreover, in Latin only words at the
+                beginning of sentences are capitalized. This means that Lorem
+                Ipsum cannot accurately represent, for example, German, in which
+                all nouns are capitalized. Thus, Lorem Ipsum has only limited
+                suitability as a visual filler for German texts. If the fill
+                text is intended to illustrate the characteristics of different
+                typefaces. It sometimes makes sense to select texts containing
+                the various letters and symbols specific to the output language.
+              </p>
+              <p>
+                One disadvantage of Lorum Ipsum is that in Latin certain letters
+                appear more frequently than others - which creates a distinct
+                visual impression. Moreover, in Latin only words at the
+                beginning of sentences are capitalized. This means that Lorem
+                Ipsum cannot accurately represent, for example, German, in which
+                all nouns are capitalized. Thus, Lorem Ipsum has only limited
+                suitability as a visual filler for German texts. If the fill
+                text is intended to illustrate the characteristics of different
+                typefaces. It sometimes makes sense to select texts containing
+                the various letters and symbols specific to the output language.
+              </p>
+              <p>
+                One disadvantage of Lorum Ipsum is that in Latin certain letters
+                appear more frequently than others - which creates a distinct
+                visual impression. Moreover, in Latin only words at the
+                beginning of sentences are capitalized. This means that Lorem
+                Ipsum cannot accurately represent, for example, German, in which
+                all nouns are capitalized. Thus, Lorem Ipsum has only limited
+                suitability as a visual filler for German texts. If the fill
+                text is intended to illustrate the characteristics of different
+                typefaces. It sometimes makes sense to select texts containing
+                the various letters and symbols specific to the output language.
+              </p>
+            </div>
           </div>
         </div>
 
@@ -153,53 +170,35 @@ export default function JobDetail() {
       </div>
 
       {/* Footer */}
-      <div></div>
-    </div>
-  );
-}
-
-function JobInformationCard({ cardData }: any) {
-  return (
-    <div
-      className={classNames(
-        `w-full bg-white shadow-md px-4 py-6 rounded-xl border sticky top-4`
-      )}
-    >
-      <h1 className={classNames(`font-semibold text-xl mb-2`)}>
-        Job Information
-      </h1>
-      <div className={classNames(`flex flex-col gap-3`)}>
-        {cardData &&
-          cardData.map((item: any) => {
-            return (
-              <JobInformationCardItem
-                icon={item.icon}
-                name={item.name}
-                value={item.value}
-              />
-            );
-          })}
-      </div>
-    </div>
-  );
-}
-interface JobInformationCardItemProps {
-  icon: React.ReactElement;
-  name: string;
-  value: string;
-}
-
-function JobInformationCardItem({
-  icon,
-  name,
-  value,
-}: JobInformationCardItemProps) {
-  return (
-    <div className={classNames(`flex flex-row items-center gap-4`)}>
-      <div className={classNames(`w-1/12 mx-2`)}>{icon}</div>
-      <div className={classNames(`flex flex-col flex-1`)}>
-        <span>{name}</span>
-        <span className={classNames(`text-teal-700`)}>{value}</span>
+      <div
+        className={classNames(
+          `flex flex-col gap-2 items-center justify-center my-12`
+        )}
+      >
+        <h1 className={classNames(`text-3xl font-semibold`)}>
+          Related Vacancies
+        </h1>
+        <h2 className={classNames(`text-lg text-zinc-500`)}>
+          Search all the open positions on the web. Get your own personalized
+          salary estimate. Read reviews on over 30000+ companies worldwide.
+        </h2>
+        <div className={classNames(`flex flex-col md:flex-row gap-6`)}>
+          {/* TODO: add job fetch data */}
+          {new Array(3)
+            .fill({
+              job: {
+                jobId: 1,
+                createdAt: new Date(),
+                jobType: "Part time",
+                name: "Frontend Developer Intern",
+                location: "HCMC",
+                quantity: 10,
+              },
+            })
+            .map((data) => {
+              return <JobCard job={data} />;
+            })}
+        </div>
       </div>
     </div>
   );
