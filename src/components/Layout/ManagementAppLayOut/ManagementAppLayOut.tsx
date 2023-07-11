8bf218cdc5647cc4f636182e06368e2741a4a445
Higo <phucnguyen0942@gmail.com>
2023-07-11 16:36:22 +0700

    other: fix layout dashboard

--- src/components/Layout/ManagementAppLayOut/ManagementAppLayOut.tsx
+++ src/components/Layout/ManagementAppLayOut/ManagementAppLayOut.tsx
@@ -1,16 +1,99 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './ManagementAppLayOut.scss';
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, NavLink } from 'react-router-dom';
 import classnames from 'classnames';
-import { Bars3Icon, XMarkIcon, PencilSquareIcon, UserIcon, ClipboardDocumentListIcon } from '@heroicons/react/24/outline';
+import { Bars3Icon, XMarkIcon, } from '@heroicons/react/24/outline';
+import { HiOutlineFolder, HiOutlineCalendarDays, HiOutlineUser, HiOutlineChartPie, HiOutlineDocumentDuplicate, HiOutlineClipboardDocument, HiOutlineClipboardDocumentList } from "react-icons/hi2"
+import { MdOutlineEventAvailable, MdOutlineManageAccounts } from "react-icons/md";
 import RecFooter from "../../RecFooter/DashboardFooter"
+export const links = [
+    {
+        title: 'ADMIN',
+        links: [
+            {
+                name: 'Default',
+                icon: <HiOutlineChartPie />,
+                mylink: '#',
+            },
+            {
+                name: 'Profile',
+                icon: <HiOutlineDocumentDuplicate />,
+                mylink: '#',
+            },
+            {
+                name: 'Manager Job',
+                icon: <HiOutlineCalendarDays />,
+                mylink: '#',
+            },
+        ],
+    },
+    {
+        title: 'RECRUITER',
+        links: [
+            {
+                name: 'Default',
+                icon: <HiOutlineChartPie />,
+                mylink: 'recruiter/dashboard',
+            },
+            {
+                name: 'Calender',
+                icon: <HiOutlineCalendarDays />,
+                mylink: 'recruiter/calender',
+            },
+            {
+                name: 'Interviewer',
+                icon: <MdOutlineManageAccounts />,
+                mylink: 'recruiter/interviewer',
+            },
+            {
+                name: 'Candidate',
+                icon: <HiOutlineUser />,
+                mylink: 'recruiter/candidate',
+            },
+            {
+                name: 'Job',
+                icon: <HiOutlineFolder />,
+                mylink: 'recruiter/job-management',
+            },
+            {
+                name: 'Event',
+                icon: <MdOutlineEventAvailable />,
+                mylink: 'recruiter/event',
+            },
+
+        ],
+    },
+    {
+        title: 'INTERVIEWER',
+        links: [
+            {
+                name: 'Interview Recent',
+                icon: <HiOutlineClipboardDocumentList />,
+                mylink: 'interviewer/interview-recent',
+            },
+            {
+                name: 'Candidate Recent',
+                icon: <HiOutlineCalendarDays />,
+                mylink: 'interviewer/candidate-recent',
+            },
+            {
+                name: 'Interview Question',
+                icon: <HiOutlineClipboardDocument />,
+                mylink: 'interviewer/interview-question',
+            },
+        ],
+    },
+];
 const ManagementAppLayOut = () => {
     const [leftActive, setLeftActive] = useState<boolean>(false);
     const [rightActive, setRightActive] = useState<boolean>(false);
+    const activeLink = 'flex items-center gap-3 pl-4 py-1.5 rounded-lg text-black text-md m-2 bg-gray-200'
+    const normalLink = ' flex items-center gap-3 pl-4 py-1.5 rounded-lg text-black text-md text-gray-700 hover:bg-gray-200 m-2'
+
     return (
         <div className="ManagementAppLayOut">
             <div className='navbar'>
-                <div className={classnames('navbar-content flex items-center justify-between', {'minimize-content': leftActive})}>
+                <div className={classnames('navbar-content flex items-center justify-between', { 'minimize-content': leftActive })}>
                     <div className='navbar-content-left flex justify-between'>
                         <button type="button" onClick={() => setLeftActive(!leftActive)}>
                             <Bars3Icon className="w-5 h-5 mr-2" />
@@ -25,99 +108,47 @@ const ManagementAppLayOut = () => {
                         </button>
                     </div>
                 </div>
-                <div className={classnames('navbar-left', {'minimize-left' : leftActive})}>
-                        <div className='flex mt-3 ml-3 mb-6'>
-                            <div className='avt flex'>
-                                <img src='https://img.hoidap247.com/picture/question/20200508/large_1588936738888.jpg'
-                                    alt='' className='avt-img' />
-                                <div className='avt-act'></div>
+                <div className={classnames('navbar-left ', { 'minimize-left': leftActive })}>
+                    <div className='flex mt-3 ml-3 mb-6'>
+                        <div className='avt flex'>
+                            <img src='https://img.hoidap247.com/picture/question/20200508/large_1588936738888.jpg'
+                                alt='' className='avt-img' />
+                            <div className='avt-act'></div>
+                        </div>
+                        <div className={classnames('mt-2 ml-2', { 'hidden': leftActive })}>Justin Bieber</div>
+                    </div>
+                    <div className=''>
+                        {links.map((item) => (
+                            <div key={item.title}>
+                                <p className={classnames('text-gray-400 mx-3 mt-4 text-x font-semibold', { 'hidden': leftActive })}>{item.title}</p>
+                                {item.links.map((link) => (
+                                    <NavLink
+                                        to={`/${link.mylink}`}
+                                        key={link.name}
+                                        onClick={() => { }}
+                                        className={({ isActive }) =>
+                                            isActive ? activeLink : normalLink}
+                                    >
+                                        <span className='text-black ml-1 text-2xl'>{link.icon}</span>
+                                        <div className={classnames({ 'text-black flex, hidden': leftActive })}>{link.name}</div>
+                                    </NavLink>
+                                ))}
                             </div>
-                            <div className={classnames('mt-2 ml-2', {'hidden' : leftActive})}>Justin Bieber</div>
-                        </div>
-                            {/* gach ngang */}
-                        <div>
-                            <div className={classnames('ml-3 text-xl text-gray-300',{'hidden' : leftActive})}>Interviewer</div>
-                            <ul>
-                                <li>
-                                    <Link to="/interviewer/interview-recent" className='flex'>
-                                        <ClipboardDocumentListIcon className="w-5 h-5 mr-2"/>
-                                        <div className={classnames({'hidden' : leftActive})}>Interview Recent</div>
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link to="/interviewer/candidate-recent" className='flex'>
-                                        <UserIcon className="w-5 h-5 mr-2" />
-                                        <div className={classnames({'hidden' : leftActive})}>Candidate Recent</div>
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link to="/interviewer/interview-question" className='flex'>
-                                        <PencilSquareIcon className="w-5 h-5 mr-2" />
-                                        <div className={classnames({'hidden' : leftActive})}>Interview Question</div>
-                                    </Link>
-                                </li>
-                            </ul>
-                        </div>
-                            {/* gach ngang */}
-                        <div>
-                            <div className={classnames('ml-3 text-xl text-gray-300',{'hidden' : leftActive})}>Interviewer</div>
-                            <ul>
-                                <li>
-                                    <Link to="/interviewer/interview-recent" className='flex'>
-                                        <ClipboardDocumentListIcon className="w-5 h-5 mr-2"/>
-                                        <div className={classnames({'hidden' : leftActive})}>Interview Recent</div>
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link to="/interviewer/candidate-recent" className='flex'>
-                                        <UserIcon className="w-5 h-5 mr-2" />
-                                        <div className={classnames({'hidden' : leftActive})}>Candidate Recent</div>
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link to="/interviewer/interview-question" className='flex'>
-                                        <PencilSquareIcon className="w-5 h-5 mr-2" />
-                                        <div className={classnames({'hidden' : leftActive})}>Interview Question</div>
-                                    </Link>
-                                </li>
-                            </ul>
-                        </div>
-                        <div>
-                            <div className={classnames('ml-3 text-xl text-gray-300',{'hidden' : leftActive})}>Interviewer</div>
-                            <ul>
-                                <li>
-                                    <Link to="/interviewer/interview-recent" className='flex'>
-                                        <ClipboardDocumentListIcon className="w-5 h-5 mr-2"/>
-                                        <div className={classnames({'hidden' : leftActive})}>Interview Recent</div>
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link to="/interviewer/candidate-recent" className='flex'>
-                                        <UserIcon className="w-5 h-5 mr-2" />
-                                        <div className={classnames({'hidden' : leftActive})}>Candidate Recent</div>
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link to="/interviewer/interview-question" className='flex'>
-                                        <PencilSquareIcon className="w-5 h-5 mr-2" />
-                                        <div className={classnames({'hidden' : leftActive})}>Interview Question</div>
-                                    </Link>
-                                </li>
-                            </ul>
-                        </div>
+                        ))}
+                    </div>
                 </div>
-                <div className={classnames('navbar-right', {'show-right' : rightActive})}>
+                <div className={classnames('navbar-right', { 'show-right': rightActive })}>
                     <div className=''>Activites</div>
                     <ul className='side-links'>
                         <button type="button" className="right-hide-btn" onClick={() => setRightActive(false)}>
-                            <XMarkIcon className="w-5 h-5 text-white"/>
+                            <XMarkIcon className="w-5 h-5 text-white" />
                         </button>
                     </ul>
                 </div>
             </div>
-            <div className={`${leftActive?'small':'large'}`}>
-                <div className='mt-5 mx-[2rem]'><Outlet/></div>
-                <RecFooter check={leftActive?true:false}/>
+            <div className={`${leftActive ? 'small' : 'large'}`}>
+                <div className='mt-5 mx-[2rem] min-h-[calc(100vh-72px-3rem)]'><Outlet /></div>
+                <RecFooter check={leftActive ? true : false} />
             </div>
         </div>
     );
--- src/components/RecFooter/DashboardFooter.tsx
+++ src/components/RecFooter/DashboardFooter.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import home from '../../pages/Home/Home'
-import styled from '@emotion/styled/macro'
+
 
 export default function DashboardFooter(props : any) {
   const style = {
@@ -8,7 +8,7 @@ export default function DashboardFooter(props : any) {
   };
   return (
     <>
-      <footer className="bg-white pb-1 fixed bottom-0 flex justify-between ml-[2rem]" style={style} >
+      <footer className="bg-white pb-1 flex justify-between ml-[2rem]" style={style} >
         <div className="">
           <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="/" className="hover:underline ml-1" >Group 2</a>
           </span>
