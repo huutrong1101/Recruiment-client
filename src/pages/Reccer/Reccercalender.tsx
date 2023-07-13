c55a9850e828a682be0af7e03b7edba001b663b9
Higo <phucnguyen0942@gmail.com>
2023-07-13 13:48:12 +0700

    add calendar

--- src/pages/Reccer/Reccercalender.tsx
+++ src/pages/Reccer/Reccercalender.tsx
@@ -1,11 +1,67 @@
-import React from 'react'
+import { useState } from 'react'
+import FullCalendar from '@fullcalendar/react'
+import { formatDate } from '@fullcalendar/core';
+import dayGridPlugin from "@fullcalendar/daygrid"
+import interactionPlugin from "@fullcalendar/interaction"
+import timeGridPlugin from "@fullcalendar/timegrid"
+import listPlugin from '@fullcalendar/list'
+import { Box, List, ListItem, ListItemText, Typography, useTheme } from '@mui/material'
 
-const Reccer_calender = () => {
+
+const Reccercalender = () => {
+
+  const [currentEvents, setCurrentEvents] = useState([])
+  const handleDateClick = (selected: any) => {
+    const title = prompt("Please enter a new title for your event")
+    const calendarApi = selected.view.calendar
+    calendarApi.unselect()
+
+    if (title) {
+      calendarApi.addEvent({
+        id: `${selected.dateStr}-${title}`,
+        title,
+        start: selected.startStr,
+        end: selected.endStr,
+        allDay: selected.allDay
+
+      })
+    }
+  }
+  const handleEventClick = (selected: any) => {
+    if (
+      window.confirm(`Are you sure yo delete the event '${selected.event.title}'`)
+
+    ) {
+      selected.event.remove()
+    }
+  }
+
   return (
-    <div>
-      Day la lich
+    <div className="calender-container pb-10">
+      <div>
+        <FullCalendar
+          plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
+          headerToolbar={{
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay'
+
+          }}
+          height={'96vh'}
+          allDaySlot={false}
+          initialView='dayGridMonth'
+          slotDuration={"01:00:00"}
+          editable={true}
+          selectable={true}
+          selectMirror={true}
+          dayMaxEvents={true}
+          weekends={true}
+          nowIndicator={true}
+          initialEvents={[]}
+        />
+      </div>
     </div>
   )
 }
 
-export default Reccer_calender
+export default Reccercalender
