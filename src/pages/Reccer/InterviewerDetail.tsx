055abb41f7ebe931d4c966aad641c0f347f72a42
Higo <phucnguyen0942@gmail.com>
2023-07-13 08:48:31 +0700

    fix layout

--- src/pages/Reccer/InterviewerDetail.tsx
+++ src/pages/Reccer/InterviewerDetail.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import InterviewerRecent from '../../pages/Interviewer/InterviewRecent/InterviewRecent'
+import { data } from "../../data/RecInterviewerDetailData"
+import RecInterviewerDetailCard from '../../components/RecInterviewerManageCard/RecInterviewerDetailCard'
 export default function InterviewerDetail() {
   return (
     <div>
-      {/* <InterviewerRecent/> */}
+      <div className='bg-white'>
+        {/* <!-- Card --> */}
+        {
+          data.map((items, index) => (
+            <div key={index} className=''>
+              <RecInterviewerDetailCard userinfor={items.userinfor} listSkills={items.listSkills} personalDetails={items.personalDetails} />
+            </div>
+          ))}
+      </div>
+      <div className='pb-10'><InterviewerRecent /></div>
+
     </div>
   )
 }
