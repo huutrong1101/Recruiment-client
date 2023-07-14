d44d5a85e4a8cec84418f8dc0d427f4002d19905
Player Nguyên <daudaua3782@gmail.com>
2023-07-14 12:54:08 +0700

    fix: PrimaryButton component

--- src/components/PrimaryButton/PrimaryButton.tsx
+++ src/components/PrimaryButton/PrimaryButton.tsx
@@ -3,28 +3,31 @@ import classnames from "classnames";
 export interface PrimaryButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   text: string;
+  className?: string;
+  size?: "sm" | "md" | "lg" | "xl";
 }
 
 export default function PrimaryButton({
   text,
+  className,
+  size,
   ...children
 }: PrimaryButtonProps) {
   return (
     <button
       className={classnames(
-        `Button w-full h-12 bg-emerald-600 hover:bg-emerald-800 `,
+        `Button bg-emerald-600 hover:bg-emerald-800 text-white`,
         `transition-colors ease-in-out duration-100`,
         `rounded-lg flex-col justify-center items-center inline-flex`,
+        "text-base px-4 py-2 w-full",
+        {
+          "text-sm px-4 py-2": size === "sm",
+        },
+        className,
       )}
       {...children}
     >
-      <div className="Basebutton px-5 py-2.5 justify-center items-center inline-flex">
-        <div className="flex items-center justify-center gap-2 Content">
-          <div className="font-semibold leading-7 tracking-wide text-white capitalize Button">
-            {text}
-          </div>
-        </div>
-      </div>
+      {text}
     </button>
   );
 }
