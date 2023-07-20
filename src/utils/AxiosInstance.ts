badbfdda79e35c8d9e730455da352cf4bdf356e7
Player Nguyên <daudaua3782@gmail.com>
2023-07-20 09:56:28 +0700

    feat: add Axios intercceptor to catch errors

--- src/utils/AxiosInstance.ts
+++ src/utils/AxiosInstance.ts
@@ -1,7 +1,23 @@
 import axios from "axios";
+import { toast } from "react-toastify";
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
+
+axiosInstance.interceptors.response.use(
+  function (response) {
+    //
+    return response;
+  },
+  function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    toast.error(`There was an error: (${error.message})`);
+    console.log(error);
+
+    return Promise.reject(error);
+  },
+);
 
 export default axiosInstance;
