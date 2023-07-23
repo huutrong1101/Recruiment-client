b71e55565a2afeac7d6f7b095ade851dbe2e96d5
Player Nguyên <daudaua3782@gmail.com>
2023-07-23 21:30:29 +0700

    feat: integrate authorization into AxiosInstance

--- src/utils/AxiosInstance.ts
+++ src/utils/AxiosInstance.ts
@@ -1,8 +1,16 @@
 import axios from "axios";
 import { toast } from "react-toastify";
+import { getLocalToken, hasLocalToken } from "./localToken";
+
+const headers = hasLocalToken()
+  ? {
+      Authorization: `Bearer ${getLocalToken()}`,
+    }
+  : {};
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
+  headers,
 });
 
 axiosInstance.interceptors.response.use(
