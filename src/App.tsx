2824b2ecfa36b8020db69874f731fe39b7972302
Trong <huutrong1101@gmail.com>
2023-07-24 10:24:16 +0700

    Fetch Data JobsPage JobDetailPage

--- src/App.tsx
+++ src/App.tsx
@@ -65,11 +65,18 @@ import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "./hooks/hooks";
 import { authLogout, fetchUserFromToken } from "./redux/AuthSlice";
 import { useTokenAuthorize } from "./hooks/useTokenAuthorize";
-import { EventService } from "./services/JobService";
+// import { EventService } from "./services/JobService";
 import UserProfileMyInformation from "./pages/UserProfile/UserProfileMyInformation";
+import { JobService } from "./services/JobService";
 
 export default function App() {
   useTokenAuthorize();
+
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    JobService.getJobs(dispatch);
+  }, []);
 
   return (
     <BrowserRouter>
