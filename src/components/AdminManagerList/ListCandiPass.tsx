5128199719394a11ee6165205cb2bc976af0c46d
Đang Nam Thien Nhan <nhan.dangruacon1304@hcmut.edu.vn>
2023-07-31 15:29:17 +0700

    feat/api-event-recruiter-post-acount-admin

--- src/components/AdminManagerList/ListCandiPass.tsx
+++ src/components/AdminManagerList/ListCandiPass.tsx
@@ -16,6 +16,8 @@ export type QueryConfig = {
 };
 import axiosInstance from "../../utils/AxiosInstance";
 import Paginationpasslist from "./Pagination/Paginationpasslist";
+import Loader from "../Loader/Loader";
+import LoadSpinner from "../LoadSpinner/LoadSpinner";
 
 export default function ListCandiPass() {
 
@@ -27,14 +29,14 @@ export default function ListCandiPass() {
   const queryConfig: QueryConfig = omitBy(
     {
       page: queryParams.page || "1",   
-      size: queryParams.size || 10,  
+      size: queryParams.size || 7,  
     },
     isUndefined,
   );
   const [prevQueryConfig, setPrevQueryConfig] =
     useState<QueryConfig>(queryConfig);
   const [pageSize, setPageSize] = useState(
-    Math.ceil(totalListJobs / Number(queryParams.size || 10)),
+    Math.ceil(totalListJobs / Number(queryParams.size || 7)),
   );
   const [showJobLists, setAdminManagerPassList] = useState(jobs);
 
@@ -68,24 +70,52 @@ export default function ListCandiPass() {
     fetchPosition();
   }, []);
 
-
-  return (
-      <div className="mt-5 elative rounded-lg">
+  if (pageSize < 1) {
+      return (
+        <TableContainer component={Paper} sx={{ border: '1px solid rgba(0, 0, 0, 0.4)'}}>
+        <Table className="text-sm text-gray-500 dark:text-gray-400 text-center">
+          <TableHead className="text-xs text-gray-700 uppercase bg-gray-200 text-center">
+            <TableRow>
+            <TableCell scope="col" className="px-1 py-1">
+                Name
+              </TableCell>
+              <TableCell scope="col" className="px-1 py-1">
+                Phone
+              </TableCell>
+              
+              <TableCell scope="col" className="px-1 py-1">
+                Date created
+              </TableCell>
+              <TableCell scope="col" className="px-1 py-1">
+                Point
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          </Table>
+          <div className="flex justify-center">
+            <Loader />
+          </div>
+          </TableContainer>  
+           
+      )
+  }
+  else  return (
+    <div className="flex-col mt-3">
     <TableContainer component={Paper} sx={{ border: '1px solid rgba(0, 0, 0, 0.4)'}}>
       <Table className="text-sm text-gray-500 dark:text-gray-400 text-center sticky">
-        <TableHead className="text-xs text-gray-700 uppercase bg-gray-200 text-center">
+        <TableHead className="text-xs text-gray-700 bg-gray-200 text-center"style={{ fontFamily: "Outfit, sans-serif" }}>
           <TableRow>
-          <TableCell scope="col" className="px-3 py-1">
+          <TableCell scope="col" className="px-1 py-1">
               Name
             </TableCell>
-            <TableCell scope="col" className="px-3 py-1">
+            <TableCell scope="col" className="px-1 py-1">
               Phone
             </TableCell>
             
-            <TableCell scope="col" className="px-3 py-1">
+            <TableCell scope="col" className="px-1 py-1">
               Date created
             </TableCell>
-            <TableCell scope="col" className="px-2 py-1">
+            <TableCell scope="col" className="px-1 py-1">
               Point
             </TableCell>
           </TableRow>
@@ -93,10 +123,10 @@ export default function ListCandiPass() {
         <TableBody>
         {showJobLists.map((job)  => (
             <TableRow className="text-black bg-white text-center" >
-              <TableCell scope="row" className="px-2 py-1 font-medium text-gray-900 whitespace-nowrap">   {job.name}  </TableCell>
-              <TableCell className="px-2 py-1">{job.phone}</TableCell>
-              <TableCell className="px-2 py-1">{job.date}</TableCell>
-              <TableCell className="px-2 py-1">{job.score}</TableCell>
+              <TableCell scope="row" className="px-1 py-1 font-medium text-gray-900 whitespace-nowrap">   {job.name}  </TableCell>
+              <TableCell className="px-1 py-1">{job.phone}</TableCell>
+              <TableCell className="px-1 py-1">{job.date}</TableCell>
+              <TableCell className="px-1 py-1">{job.score}</TableCell>
             </TableRow>
           ))}
         </TableBody>
