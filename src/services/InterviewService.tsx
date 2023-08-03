288b77177e6658acb366b832476626728669d5ef
Huy Trinh <huy.trinh20102001@gmail.com>
2023-08-03 16:44:34 +0700

    fix conflicts in my branch

--- src/services/InterviewService.tsx
+++ src/services/InterviewService.tsx
@@ -1,14 +1,18 @@
 import axiosInstance from "../utils/AxiosInstance";
 
-const createInterview = async (data: any) => {
-  return await axiosInstance.post(`/recruiter/interviews`, data);
+// const createInterview = async (data: any) => {
+//   return await axiosInstance.post(`/recruiter/interviews`, data);
+// };
+
+const createQuestion = async (data: any) => {
+  return await axiosInstance.post(`interviewer/question`, data);
 };
 
 const updateQuestion = async (data:any, questionID:string ) => {
-  return await axiosInstance.put(`/recruiter/interviews/${questionID}`, data);
+  return await axiosInstance.put(`interviewer/question/${questionID}`, data);
 }
 
 export const InterviewService = {
-  createInterview,
+  createQuestion,
   updateQuestion,
 };
