a5db5cd50b46ebd599c7c8ef3d217521f3b344f5
Le Thanh Khai <khai.lethanh@hcmut.edu.vn>
2023-08-03 10:03:32 +0700

    fix: panic

--- src/redux/reducer/CandidateListSlice.tsx
+++ src/redux/reducer/CandidateListSlice.tsx
@@ -21,15 +21,19 @@ const CandidateListSlice = createSlice({
     setCandidateTotal(state, action) {
       state.candidateTotal = action.payload;
     },
-    setCandidateskillList(state,action){
-      state.skill=action.payload
-  }
+    setCandidateskillList(state, action) {
+      state.skill = action.payload;
+    },
   },
 });
 
 export default CandidateListSlice.reducer;
-export const { setCandidatesList, setCandidatesListStatus, setCandidateTotal, setCandidateskillList } =
-  CandidateListSlice.actions;
+export const {
+  setCandidatesList,
+  setCandidatesListStatus,
+  setCandidateTotal,
+  setCandidateskillList,
+} = CandidateListSlice.actions;
 
 export const fetchCandidateList = () => {
   return async function fetchCandidateListThunk(dispatch: Dispatch) {
@@ -38,6 +42,7 @@ export const fetchCandidateList = () => {
       const reponse = await axiosInstance.get(`recruiter/applied-candidates`);
       const data = await reponse.data;
       dispatch(setCandidatesList(data.result.content));
+      dispatch(setCandidateTotal(data.result.totalElements));
       dispatch(setCandidatesListStatus(STATUS.IDLE));
     } catch (error) {
       dispatch(setCandidatesListStatus(STATUS.ERROR));
@@ -45,16 +50,16 @@ export const fetchCandidateList = () => {
   };
 };
 
-export const fetchCandidateSkill =()=>{
-  return async function fetchCandidatesSkillThunk(dispatch:Dispatch){
-      dispatch(setCandidatesListStatus(STATUS.LOADING))
-      try{
-          const response = await axiosInstance.get(`candidate/skills`);
-          const data = await response.data
-          dispatch(setCandidateskillList(data.result))
-          dispatch(setCandidatesListStatus(STATUS.IDLE))
-      } catch(error){
-          dispatch(setCandidatesListStatus(STATUS.ERROR))
-      }
-  }
-}
+export const fetchCandidateSkill = () => {
+  return async function fetchCandidatesSkillThunk(dispatch: Dispatch) {
+    dispatch(setCandidatesListStatus(STATUS.LOADING));
+    try {
+      const response = await axiosInstance.get(`candidate/skills`);
+      const data = await response.data;
+      dispatch(setCandidateskillList(data.result));
+      dispatch(setCandidatesListStatus(STATUS.IDLE));
+    } catch (error) {
+      dispatch(setCandidatesListStatus(STATUS.ERROR));
+    }
+  };
+};
