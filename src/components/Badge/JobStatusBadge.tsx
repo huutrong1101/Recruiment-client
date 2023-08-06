b328e1022ffbf7608f9ac5e0ce1a1152f1f39699
Player Nguyên <daudaua3782@gmail.com>
2023-08-06 23:23:07 +0700

    fix: patch pill passed not working

--- src/components/Badge/JobStatusBadge.tsx
+++ src/components/Badge/JobStatusBadge.tsx
@@ -5,7 +5,7 @@ import { APPLICANTS_STATUS } from "../../utils/Localization";
 
 interface JobStatusBadge extends React.HTMLProps<HTMLDivElement> {
   className?: string;
-  status: "NOT_RECEIVED" | "RECEIVED" | "PASS" | "FAILED";
+  status: "NOT_RECEIVED" | "RECEIVED" | "PASSED" | "FAILED";
 }
 
 export default function JobStatusBadge({ className, status }: JobStatusBadge) {
@@ -14,7 +14,7 @@ export default function JobStatusBadge({ className, status }: JobStatusBadge) {
       className={classnames(
         `px-2 w-32`,
         {
-          "bg-emerald-600": status === "PASS",
+          "bg-emerald-600": status === "PASSED",
           "bg-yellow-600": status === "NOT_RECEIVED",
           "bg-orange-600": status === "RECEIVED",
           "bg-red-600": status === "FAILED",
@@ -25,7 +25,7 @@ export default function JobStatusBadge({ className, status }: JobStatusBadge) {
     >
       <span
         className={classnames("h-2 w-2 rounded-xl", {
-          "bg-emerald-800": status === "PASS",
+          "bg-emerald-800": status === "PASSED",
           "bg-yellow-800": status === "NOT_RECEIVED",
           "bg-orange-800": status === "RECEIVED",
           "bg-red-800": status === "FAILED",
@@ -33,7 +33,7 @@ export default function JobStatusBadge({ className, status }: JobStatusBadge) {
       ></span>
       <span
         className={classnames({
-          "text-emerald-300": status === "PASS",
+          "text-emerald-300": status === "PASSED",
           "text-yellow-300": status === "NOT_RECEIVED",
           "text-orange-300": status === "RECEIVED",
           "text-red-300": status === "FAILED",
